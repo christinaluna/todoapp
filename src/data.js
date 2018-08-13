289146e578ceccb5df81f50434efcdc32988ee5d
Cristina Luna <christina.luna@meltmedia.com>
2018-08-13 15:49:08 -0700

    Adds missing update and delete functions

--- src/data.js
+++ src/data.js
@@ -31,3 +31,32 @@ data.readToDo = function(id) {
     }
   }
 };
+
+// Updates to do item
+data.updateToDo = function(id, title, complete) {
+  var targetIndex;
+  for (var i = 0; i < data.store.length; i++) {
+    if (data.store[i].id === id) {
+      targetIndex = i;
+      break;
+    }
+  };
+  if (targetIndex > -1) {
+      data.store[targetIndex].title = title;
+      data.store[targetIndex].complete = complete;
+  }
+};
+
+// Deletes to do item
+data.deleteToDo = function(id) {
+  var targetIndex;
+  for (var i = 0; i < data.store.length; i++) {
+    if (data.store[i].id === id) {
+      targetIndex = i;
+      break;
+    }
+  };
+  if (targetIndex > -1) {
+    data.store.splice(targetIndex, 1);
+  }
+};
